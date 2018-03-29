e762ffa98d5b998ac75530d2d0d23a7103725006
(null) McNull <noreplies@hotmail.com>
2018-03-29 12:58:17 +0200

    happy love buttons

--- src/app/core/toolbar/toolbar-service.js
+++ src/app/core/toolbar/toolbar-service.js
@@ -8,20 +8,32 @@ function appToolbar() {
   /* @ngInject */
   class AppToolbar {
     constructor() {
-
       this.title = 'Angular App';
       this.buttons = [
-        {
-          icon: 'fa fa-heart-o',
-          onClick: ($event, button) => { console.log($event, button); }
-        }
         /*
         {
           icon: 'fa fa-love',
-          onClick: ($event, icon) => {}
+          onClick: ($event, icon) => {},
+          order: 100
         }
         */
       ];
+    }
+
+    addButton(button) {
+      button.order = button.order || 100;
+      this.buttons.push(button);
+      this.buttons.sort((x, y) => x.order - y.order);
+    }
+
+    removeButton(button) {
+      const idx = this.buttons.indexOf(button);
+
+      if(idx === -1) {
+        throw new Error('Button not found');
+      }
+
+      this.buttons.splice(idx, 1);
     }
   }
 
--- src/app/home/index.js
+++ src/app/home/index.js
@@ -4,9 +4,47 @@ import { addRoute } from '../core';
 addRoute({
   url: '/',
   template: require('./home.html'),
-  // controller: ($scope) => {
-  //   /*@ngInject*/
-  // }
+  controller: /* @ngInject */ ($scope, appToolbar) => {
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    let hasLove = false;
+
+    const loveButton = {
+      icon: 'fa fa-heart-o',
+      onClick: () => {
+        hasLove = !hasLove;
+        loveButton.icon = 'fa ' + (hasLove ? 'fa-heart' : 'fa-heart-o');
+      }
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    let happy = true;
+
+    const happyButton = {
+      icon: 'fa fa-smile-o',
+      order: 10,
+      onClick: () => {
+        happy = !happy;
+        happyButton.icon = 'fa ' + (happy ? 'fa-smile-o' : 'fa-frown-o');
+      }
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    appToolbar.addButton(loveButton);
+    appToolbar.addButton(happyButton);
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    $scope.$on('$destroy', () => {
+      appToolbar.removeButton(loveButton);
+      appToolbar.removeButton(happyButton);
+    });
+
+    ////////////////////////////////////////////////////////////////////////////
+  }
 });
 
 angular.module('app.home', [
